685b7cc8bf86249b0926ed3f1e34c2dc46309615
Chouffe <arthur@caillau.me>
2024-04-24 13:45:40 +0200

    [scripts] add train stage to dvc

--- pyronear_mlops/model/yolov8/train.py
+++ pyronear_mlops/model/yolov8/train.py
@@ -17,10 +17,24 @@ def train(
 ):
     """Main function for running a train run."""
     assert data_yaml_path.exists(), f"data_yaml_path does not exist, {data_yaml_path}"
+    default_params = {
+        "batch": 16,
+        "epochs": 100,
+        "patience": 100,
+        "imgsz": 640,
+        # data augmentation
+        "close_mosaic": 10,
+        "degrees": 0.0,
+        "translate": 0.1,
+        "flipud": 0.0,
+        "fliplr": 0.5,
+    }
+    params = {**default_params, **params}
     model.train(
         project=project,
         name=experiment_name,
         data=data_yaml_path.absolute(),
+        # data=data_yaml_path,
         epochs=params["epochs"],
         imgsz=params["imgsz"],
         close_mosaic=params["close_mosaic"],
